729ed3226777949e759c3c9315247c039731213d
Yuki Sekido <se_yuuki@the-cnt.tepm.jp>
2021-01-06 09:02:51 +0000

    Add a file.

---
+++ scripts/tentimes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int32
+
+n = 0
+
+def cb(message):
+    global n
+    n = message.data*10
+
+rospy.init_node('tentimes')
+sub = rospy.Subscriber('count_up', Int32, cb)
+pub = rospy.Publisher('tentimes', Int32, queue_size=1)
+rate = rospy.Rate(1)
+while not rospy.is_shutdown():
+    pub.publish(n)
+    rate.sleep()
+
